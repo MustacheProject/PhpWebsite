1c2b5943fc2820aeee7321af9fb356334db8acea
Eugenio <eugenio.piccirillo@gmail.com>
2017-11-18 13:09:35 +1100

    Fixed time
    fixed time in about

--- About.php
+++ About.php
@@ -9,17 +9,17 @@
   <ul>
     <li>
       <div>
-        <time>1990</time> I'm just born! give a break!
+        <time class="bold">1990: </time> I'm just born! give a break!
       </div>
     </li>
     <li>
       <div>
-        <time>2009</time> I conclude my High School degree in IT with a grade of 81/100
+        <time class="bold">2009: </time> I conclude my High School degree in IT with a grade of 81/100
       </div>
     </li>
     <li>
       <div>
-        <time>2010</time> I start working on my first real job, office forker
+        <time class="bold">2010: </time> I start working on my first real job, office forker
         for shipment for a Bank. Have you ever tried to but a 20 years old-
         fresh from school guy- in a strict Bank environment? No? Well, I can
         assure that is funny, but just from the outside!
@@ -27,14 +27,14 @@
     </li>
     <li>
       <div>
-        <time>2012</time> Bought my first semi-professional reflex! It can looks
+        <time class="bold">2012: </time> Bought my first semi-professional reflex! It can looks
         like a useless milestone but it was were my passion for photo and video
          making starts!
       </div>
     </li>
     <li>
       <div>
-        <time>2013</time> I've always had passion for cooking but here I decide
+        <time class="bold">2013: </time> I've always had passion for cooking but here I decide
         to spend time and money on a professional chef course! It required me
         to sacrifice a huge part of my free time, especially when I finish the course
         and start working in kitchen, because I was helding both my jobs.
@@ -44,14 +44,14 @@
     </li>
     <li>
       <div>
-        <time>2014</time> I was thirst for experience and knowlege! I was
+        <time class="bold">2014: </time> I was thirst for experience and knowlege! I was
         learning so much of the world just changing my job and then, I decided
         was time to explore new horizons. Australia, I am coming!
       </div>
     </li>
     <li>
       <div>
-        <time>2016/2017</time> After one year in Australia working as Chef,
+        <time class="bold">2016/2017: </time> After one year in Australia working as Chef,
         I felt like was time for me to go back to my REAL and first passion, programming.
         I completed a Diploma of Software Development and an Advance Diploma of IT
          in order to get back all the knowlege I had and learn even more of them.
@@ -59,7 +59,7 @@
     </li>
     <li>
       <div>
-        <time>Today</time> As I have always done in my life, I am still studying
+        <time class="bold">Today: </time> As I have always done in my life, I am still studying
         every day in order to improve my ability. So far I am still looking for
          my first real job in the IT, and can't wait to start.
       </div>
